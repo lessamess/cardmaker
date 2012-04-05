8b02786eca9c6ef69331d9e8d67ac1c843c34b57
Memi Beltrame <memi.beltrame@liip.ch>
2012-04-05 18:07:06 +0200

    splitting up code, adding support for 1 and 8 cards per page

--- index.php
+++ index.php
@@ -1,27 +1,79 @@
 <?php
+
 if (!empty($_POST['cards'])) {
+
     define('FPDF_FONTPATH','fpdf/font/');
     include("fpdf/fpdf.php");
 
-    $strings = explode("\n", $_POST['cards']);
+    $strings = explode("\n", trim($_POST['cards']));
 
-    $pdf = new FPDF("L","mm","A4");
+    $showNumbers = 0;
+    if (!empty($_POST['showNumbers'])) {
+        $showNumbers = 1;
+    }
+
+    $cardsPerPage = $_POST['cardsPerPage'];
+
+    $orientation = "L";
+    if ($cardsPerPage == 8) {
+        $orientation = "P";
+    }
+
+    $pdf = new FPDF($orientation, "mm", "A4");
     $pdf->SetTopMargin = 0;
     $pdf->SetDrawColor(220, 220, 220);
 
-    // Linienbreite einstellen, 0.5 mm
     $pdf->SetLineWidth(0.1);
+    switch ($cardsPerPage){
+        case 1:
+            $pdf = oneCardPdf($pdf, $strings, $showNumbers);
+            break;
+        case 4:
+            $pdf = fourCardPdf($pdf, $strings, $showNumbers);
+            break;
+        case 8:
+            $pdf = eightCardPdf($pdf, $strings, $showNumbers);
+            break;
+    }
 
+    $pdf->Output("Cardsortdeck.pdf","D");
+
+    die();
+}
+
+function oneCardPdf($pdf, $strings, $showNumbers){
+
+        Foreach ($strings as $key => $string) {
+        $num = $key + 1;
+        $pdf->AddPage();
+        $pdf->SetXY(20, 30);
+
+        //write Number
+        if ($showNumbers == 1) {
+            $pdf->SetFont('Arial','',20);
+            $pdf->Cell( 240, 20, $num,0,2);
+        }
+        //write Card Name
+        $pdf->SetFont('Arial','B',32);
+        $pdf->MultiCell( 257, 16, utf8_decode($string),0,"L"); // write Cardname
+        //Syntax: MultiCell(float w , float h , string txt [, mixed border] [, string align] [, integer fill])
+    }
+    return $pdf;
+
+}
+function fourCardPdf($pdf, $strings, $showNumbers){
     Foreach ($strings as $key => $string) {
         $num = $key + 1;
         Switch ($num%4){
             case 1:
                 $pdf->AddPage();
 
-                // Linie zeichnen
+                // draw cropmarks
                 if(!empty($_POST['cropmark'])) {
-                    $pdf->Line(148, 0, 148, 210);
-                    $pdf->Line(0, 105, 297, 105);
+                    $pdf->Line(148, 0, 148, 20);
+                    $pdf->Line(148, 190, 148, 210);
+                    $pdf->Line(0, 105, 20, 105);
+                    $pdf->Line(277, 105, 297, 105);
                 }
 
                 $pdf->SetXY(20, 30);
@@ -36,18 +88,67 @@ if (!empty($_POST['cards'])) {
                 $pdf->SetXY(170, 125);
             break;
         }
-
-        $pdf->SetFont('Arial','',12);
-        $pdf->Cell( 110, 10, $num,0,2);
+        //write Number
+        if ($showNumbers == 1) {
+            $pdf->SetFont('Arial','',12);
+            $pdf->Cell( 110, 10, $num,0,2);
+        }
+        //write Card Name
         $pdf->SetFont('Arial','B',14);
-        $pdf->MultiCell( 110, 8, utf8_decode($string),0);
+        $pdf->MultiCell( 110, 8, utf8_decode($string),0,"L"); // write Cardname
     }
+    return $pdf;
+}
 
-    $pdf->Output("Cardsortdeck.pdf","D");
+function eightCardPdf($pdf, $strings, $showNumbers){
+    Foreach ($strings as $key => $string) {
+        $num = $key + 1;
+        Switch ($num%8){
+            case 1:
+                $pdf->AddPage();
 
+                // draw cropmarks
+                if(!empty($_POST['cropmark'])) {
+                    $pdf->Line(0, 74, 210, 74);
+                    $pdf->Line(0, 148, 210, 148);
+                    $pdf->Line(0, 222, 210, 222);
+                    $pdf->Line(105, 0, 105, 297);
+                }
 
-
-    die();
+                $pdf->SetXY(10, 10);
+                break;
+            case 2:
+                $pdf->SetXY(115, 10);
+                break;
+            case 3:
+                $pdf->SetXY(10, 82);
+                break;
+            case 4:
+                $pdf->SetXY(115, 82);
+                break;
+            case 5:
+                $pdf->SetXY(10, 154);
+                break;
+            case 6:
+                $pdf->SetXY(115, 154);
+                break;
+            case 7:
+                $pdf->SetXY(10, 226);
+                break;
+            case 0:
+                $pdf->SetXY(115, 226);
+                break;
+        }
+        //write Number
+        if ($showNumbers == 1) {
+            $pdf->SetFont('Arial','',12);
+            $pdf->Cell( 85, 10, $num,0,2);
+        }
+        //write Card Name
+        $pdf->SetFont('Arial','B',14);
+        $pdf->MultiCell( 85, 7, utf8_decode($string),0,"L"); // write Cardname
+    }
+    return $pdf;
 }
 
 ?><!DOCTYPE html>
@@ -55,102 +156,7 @@ if (!empty($_POST['cards'])) {
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Cardmaker</title>
-    <style>
-        body {
-            font-family: Georgia, "Times New Roman", serif;
-            background:url(hintergrund.jpg);
-            margin: 20px 0 20px -300px;
-            padding-left: 50%;
-            color: #c9a24f;
-        }
-        a:link {
-            color: #b50000;
-        }
-
-        a:visited {
-            color: #b50000;
-        }
-        #everything {
-            width: 600px;
-            background-color: #fff;
-            -moz-border-radius: 15px;
-                border-radius: 15px;
-            padding:22px;
-            background-color: #f7f6f2;
-            background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #f7f6f2), color-stop(13%, #fff), color-stop(25%, #f7f6f2) );
-            background-image: -moz-linear-gradient(top, #f7f6f2, #fff 13%, #f7f6f2 25%, #f7f6f2);
-            border: 1px dotted #c9a24f;
-
-        }
-
-        .footer{
-            border-bottom: 1px dotted #c9a24f;
-            padding:22px;
-            width: 600px;
-            margin-top: 20px;
-            font-family: helvetica, verdana, sans-serif;
-            font-size: 0.8em;
-            text-align: center;
-        }
-        h1 {
-            width: 100%;
-            text-align: center;
-            font-weight: normal;
-            font-size: 2.2em;
-            margin: 0;
-        }
-        textarea {
-            width: 100%;
-            height: 300px;
-            border: 1px solid #c9a24f;
-        }
-
-        .claim {
-            width: 100%;
-            text-align: center;
-        }
-        .instr {
-            color:#87AF0E;
-            width: 500px;
-            margin-left: 50px;
-            border-top: 1px dashed #c9a24f;
-            border-bottom: 1px dashed #c9a24f;
-            padding: 10px 0;
-        }
-        .button {
-            border: 1px solid #4E7D0E;
-            border-radius: 5px;
-            width: 150px;
-            display: inline-block;
-            outline: none;
-            cursor: pointer;
-            text-align: center;
-            text-decoration: none;
-            color: #fff;
-            font-size: 12px;
-            font-weight: bold;
-            padding: 7px 0;
-            background: #000;
-            background-image:
-                -webkit-gradient(
-                    linear,
-                    left top,
-                    left bottom,
-                    color-stop(0.00, #7DB72F),
-                    color-stop(100%, #4E7D0E)
-                );
-            background-image: -moz-linear-gradient(center top , #7DB72F, #4E7D0E);
-        }
-    </style>
-    <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-2297835-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+    <link href="style.css" media="screen" type="text/css" rel="stylesheet" />
 </head>
 <body>
     <div id="everything">
@@ -160,17 +166,25 @@ pageTracker._trackPageview();
         <p class="claim instr">Enter Card-Names&nbsp;&nbsp;&nbsp;→&nbsp;&nbsp;&nbsp; Get PDF&nbsp;&nbsp;&nbsp;→&nbsp;&nbsp;&nbsp;Print Your Cardsort Deck</p>
         <br>
 
-        Enter Card-Names, one name per line:
-        <form action="index.php" method="POST">
-            <textarea name="cards" ></textarea>
-            <br><br>
-            <input type="checkbox" id="cropmark" name="cropmark" value=1> <label for="cropmark">Show cropmarks</label><br><br>
-                <input type="submit" class="button" name="submit" value="Get PDF">
+        Enter Card-Names, one name per line:<br><br>
 
+        <form action="index.php" method="POST">
+            <textarea name="cards" id="cards"></textarea>
+            <br><br>
+            Cards per page:
+            <input type="radio" name="cardsPerPage" value="1"> 1 &nbsp;
+            <input type="radio" name="cardsPerPage" value="4" checked> 4 &nbsp;
+            <input type="radio" name="cardsPerPage" value="8"> 8 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="checkbox" id="showNumbers" name="showNumbers" value=1 checked> <label for="cropmark">Show numbers</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="checkbox" id="cropmark" name="cropmark" value=1> <label for="cropmark">Show cropmarks</label><br>
+            <br>
+                <input type="submit" class="button" name="submit" value="Get PDF">
         </form>
     </div>
     <div class="footer">
     Ping me on <a href="http://twitter.com/bratwurstkomet">Twitter for feedback</a>
     </div>
 <!-- PUT JS HERE -->
+<script type="text/javascript" src="analytics.js"></script>
+<script type="text/javascript" src="main.js"></script>
 </body>
