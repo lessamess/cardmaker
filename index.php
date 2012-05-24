9516cc06e41f4351cd0ca9826258059ddb57eb5c
Memi Beltrame <memi.beltrame@liip.ch>
2012-05-24 12:03:29 +0200

    minor layout fix for mobile

--- index.php
+++ index.php
@@ -220,12 +220,13 @@ function eightCardPdf($pdf, $strings, $showNumbers){
         <form action="index.php" method="POST">
             <textarea name="cards" id="cards"></textarea>
             <br><br>
-            Cards per page:
+            Cards per page:<br class="hideOnDesktop">
             <label><input type="radio" name="cardsPerPage" value="1"> 1 </label>&nbsp;
             <label><input type="radio" name="cardsPerPage" value="2"> 2 </label>&nbsp;
             <label><input type="radio" name="cardsPerPage" value="4" checked> 4 </label>&nbsp;
             <label><input type="radio" name="cardsPerPage" value="8"> 8 </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br class="hideOnDesktop">
-            Show:&nbsp;&nbsp;<input type="checkbox" id="showNumbers" name="showNumbers" value=1 checked> <label for="showNumbers">numbers</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br class="hideOnDesktop"><br class="hideOnDesktop">
+            <br class="hideOnDesktop">&nbsp;<br class="hideOnDesktop">
+            Show:&nbsp;&nbsp;<br class="hideOnDesktop"><input type="checkbox" id="showNumbers" name="showNumbers" value=1 checked> <label for="showNumbers">numbers</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br class="hideOnDesktop"><br class="hideOnDesktop">
             <input type="checkbox" id="cropmark" name="cropmark" value=1> <label for="cropmark">cropmarks</label><br>
             <br>
                 <input type="submit" class="button" name="submit" value="Get PDF">
